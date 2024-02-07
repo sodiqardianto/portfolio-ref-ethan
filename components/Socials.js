b2149c57e42bc6b0f194aa915090061710f44ead
sodiqardianto <sodiqardianto@gmail.com>
2024-02-07 10:06:03 +0700

    done kecuali service, work, testiomni

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,33 @@
+import Link from "next/link";
+
+import {
+  RiFacebookLine,
+  RiGithubLine,
+  RiInstagramLine,
+  RiLinkedinLine,
+  RiYoutubeLine,
+} from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiYoutubeLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiLinkedinLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiGithubLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
